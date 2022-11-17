6b447fff17206d82659ad0ed3b53e97d2132bcad
Devon <devonvandyk@gmail.com>
2022-11-17 11:33:55 -0400

    add star class, animation, points ui, pickup sound

--- app/space.rb
+++ app/space.rb
@@ -1,5 +1,11 @@
 require 'gosu'
 
+
+module ZOrder     #sets what elements are in front of what, ie background - 0 (behind other elements) to UI - 3 (front of screen)
+    BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
+
 class Spaceflight < Gosu::Window
     
     def initialize
@@ -10,6 +16,11 @@ class Spaceflight < Gosu::Window
 
         @player = Player.new # creates/initializes an instance of the player
         @player.warp(400, 300) # sets the player in the center of viewport ( 1/2 of 800x600 )
+
+        @star_anim = Gosu::Image.load_tiles("assets/media/star.png", 25, 25)
+        @stars = Array.new
+
+        @font = Gosu::Font.new(20)
     end
 
     def update #movement logic in here
@@ -27,12 +38,18 @@ class Spaceflight < Gosu::Window
         end
 
         @player.move
+        @player.collect_stars(@stars)
 
+        if rand(100) < 4 and @stars.size < 25
+            @stars.push(Star.new(@star_anim))
+        end
     end
 
     def draw #renders the visuals
-        @background_image.draw(0, 0, 0) # drawn at x 0 , y 0 and z-index of 0
+        @background_image.draw(0, 0, ZOrder::BACKGROUND) # drawn at x 0 , y 0 and z-order set by module
         @player.draw
+        @stars.each { |star| star.draw}
+        @font.draw("Stars Collected: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
     end
 
     def button_down(id)
@@ -42,13 +59,15 @@ class Spaceflight < Gosu::Window
             super
         end  
     end
-
 end
 
+
 class Player #Settings for player character
+    attr_reader :score
 
     def initialize
         @image = Gosu::Image.new("assets/media/player-ship.bmp")
+        @beep = Gosu::Sample.new("assets/media/star.wav")
         @x = @y = @vel_x = @vel_y = @angle = 0.0 # set player position to 0.0 to start
         @score = 0
     end
@@ -81,10 +100,51 @@ class Player #Settings for player character
     end
 
     def draw
-        @image.draw_rot(@x, @y, 1, @angle) # center of player sprite at x,y with z-index of 1 (in front of background image)
+        @image.draw_rot(@x, @y, ZOrder::PLAYER, @angle) # center of player sprite at x,y
     end
 
+    def score
+        @score
+    end
+
+    def collect_stars(stars) #removes stars from the screen after the player reaches them
+        stars.reject! do |star|
+            if Gosu.distance(@x, @y, star.x, star.y) < 35
+                @score += 1 #adds one to stars collected
+                @beep.play #plays sound after star collected
+                true
+            else
+                false
+            end
+        end
+    end
 end
+
+
+
+# Add star animations
+
+class Star
+    attr_reader :x, :y
+
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256 - 40) + 40 
+        @color.green = rand(256 - 40) + 40 
+        @color.blue = rand(256 - 40) + 40
+        @x = rand * 800 # stars will spawn at a random position within the play area
+        @y = rand * 600
+    end
+
+    def draw
+        img = @animation[Gosu.milliseconds / 100 % @animation.size] # shows a different frame every 100 milliseconds
+        img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
+        ZOrder::STARS, 1, 1, @color, :add)
+    end
+end
+
+
 
 
 
